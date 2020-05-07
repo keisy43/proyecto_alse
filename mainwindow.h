f8580f99be304caa7ebf546ae25e6db047399cc7
JeissonVelandia <64870074+JeissonVelandia@users.noreply.github.com>
2020-05-07 00:11:31 -0500

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -1,14 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QDialog>
 #include <QTimer>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow : public QDialog
 {
     Q_OBJECT
 
@@ -20,13 +20,36 @@ public:
 private slots:
     void cambio_estado(void );
 
+    void on_boton1_clicked();
 
+    void on_boton2_clicked();
+
+    void on_boton3_clicked();
+
+    void on_boton4_clicked();
+
+    void on_boton5_clicked();
+
+    void on_boton6_clicked();
+
+    void on_boton7_clicked();
+
+    void on_boton8_clicked();
+
+    void on_boton9_clicked();
+
+    void on_boton10_clicked();
+
+    void on_boton11_clicked();
 
 private:
     Ui::MainWindow *ui;
     QTimer  *_timer;
     char _estado;
+    int conteo=0;
+
     QPixmap *rd, *rl;
+
 };
 
 #endif // MAINWINDOW_H
